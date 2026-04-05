40e7b365ec896f13a6d9126365d6e76d772e2fc5
Dave Coleman <dave@dvclmn.com>
2026-04-05 11:17:23 +0200

    Continues moving relevant code over to BasePrimitives repo

--- Sources/InteractionKit/GeometryPrimitives/CGSize/CGSize.swift
+++ Sources/InteractionKit/GeometryPrimitives/CGSize/CGSize.swift
@@ -9,24 +9,24 @@ import Foundation
 
 extension CGSize {
 
-  package var midpoint: CGPoint {
-    return CGPoint(x: width / 2, y: height / 2)
-  }
-  public init(fromLength length: CGFloat) {
-    self.init(width: length, height: length)
-  }
-
-  /// Returns true if both width and height are greater than zero
-  public var isGreaterThanZero: Bool { width > 0 && height > 0 }
-
-  /// Returns true if both width and height are greater than or equal to zero
-  public var isGreaterThanOrEqualToZero: Bool { width >= 0 && height >= 0 }
-
-  /// Returns true if both width and height is zero
-  public var isZero: Bool { width == 0 && height == 0 }
-
-  /// Returns true if either width or height is zero or negative
-  public var isLessThanOrEqualToZero: Bool {
-    !isGreaterThanZero
-  }
+//  package var midpoint: CGPoint {
+//    return CGPoint(x: width / 2, y: height / 2)
+//  }
+//  public init(fromLength length: CGFloat) {
+//    self.init(width: length, height: length)
+//  }
+//
+//  /// Returns true if both width and height are greater than zero
+//  public var isGreaterThanZero: Bool { width > 0 && height > 0 }
+//
+//  /// Returns true if both width and height are greater than or equal to zero
+//  public var isGreaterThanOrEqualToZero: Bool { width >= 0 && height >= 0 }
+//
+//  /// Returns true if both width and height is zero
+//  public var isZero: Bool { width == 0 && height == 0 }
+//
+//  /// Returns true if either width or height is zero or negative
+//  public var isLessThanOrEqualToZero: Bool {
+//    !isGreaterThanZero
+//  }
 }
